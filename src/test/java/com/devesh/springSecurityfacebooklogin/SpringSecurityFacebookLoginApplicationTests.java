c3ec4aa8ef2da9151dd6adc50e28464418f91a92
Devesh Kumar <deveshkr271988@gmail.com>
2020-08-06 19:19:12 +0200

    initial checkin

---
+++ src/test/java/com/devesh/springSecurityfacebooklogin/SpringSecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesh.springSecurityfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
